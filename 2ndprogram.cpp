28f8f550598cbabffa4cb0fa539e4edb0e85ece2
Jay-Prakash-Tanty <jayprakashtanty@gmail.com>
2024-03-23 12:05:55 +0530

    2nd program of 1st  week assignment

---
+++ 2ndprogram.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,d;
+    cout<<"Enter a number to check if it is positive, negative or zero:";
+    cin >>a;
+    if(a>0)
+    {
+    cout<< a <<" is a positive number";
+    }
+    else if(a<0)
+    {
+    cout<< a <<" is a negative number";
+    }
+    else if(a==0)
+    {
+    cout<< a <<" is zero";
+    }
+    return 0;
+}
